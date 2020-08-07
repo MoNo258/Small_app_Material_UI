df1c5e730eee45ccf9f5ddc3358071face45e30b
Monika N <monika258@gmail.com>
2020-08-07 15:41:28 +0200

    done with basic view

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import './App.css';
+import Home from "./pages/Home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          This is small app with Material UI.
-        </p>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Home/>
+    )
+};
 
 export default App;
